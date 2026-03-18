457e1f8a604ccffd4ea4f7d13d2d75a634672a35
hewigovens <360470+hewigovens@users.noreply.github.com>
2026-03-18 16:49:47 +0900

    update app detective to 1.3.0

--- Casks/app-detective.rb
+++ Casks/app-detective.rb
@@ -1,6 +1,6 @@
 cask "app-detective" do
-  version "1.2.0"
-  sha256 "c1ef344fbd06745ed45ad8cbb972d474ea703011fdfbabcc9faca9a49fe496d4"
+  version "1.3.0"
+  sha256 "83ccc09f3963a3b7a4ab5f1238ad20608135cb10807f4fd465113e572c10a043"
 
   url "https://github.com/hewigovens/app-detective/releases/download/#{version}/AppDetective-#{version}.zip"
   name "App Detective"
