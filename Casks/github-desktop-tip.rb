ab7c01b26db1addca4e204ae00406d60bfc5dab4
hewigovens <360470+hewigovens@users.noreply.github.com>
2026-04-09 10:56:36 +0900

    update sha256

--- Casks/github-desktop-tip.rb
+++ Casks/github-desktop-tip.rb
@@ -1,6 +1,6 @@
 cask "github-desktop-tip" do
   version "20260409-fcaca2e6b4"
-  sha256 "a9883548af4dd8de816b0d385d6a9a91bd31fcb9a4ba0e1e0b982afdf21ecee6"
+  sha256 "695f2a4df6a7b4a88d969aa04986b6f14857cae4078ff405b6283898c4d434e9"
 
   url "https://github.com/hewigovens/github-desktop/releases/download/tip-#{version}/GitHub.Desktop-arm64.zip"
   name "GitHub Desktop (Tip)"
