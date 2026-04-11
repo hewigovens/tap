2aacea86e4c0af0dabfa1fe5ab022c2e28762fc4
hewigovens <360470+hewigovens@users.noreply.github.com>
2026-04-11 09:37:56 +0900

    jayjay 0.2.9 (build 13)

--- Casks/jayjay.rb
+++ Casks/jayjay.rb
@@ -1,6 +1,6 @@
 cask "jayjay" do
-  version "0.2.8,12"
-  sha256 "710cc4ca7aad1477d4e8c50dba063b21fbcd46a8cb7ba8eae0d3c17f8917d332"
+  version "0.2.9,13"
+  sha256 "e948823bfda13562086bcad8e27ad55b33a2564c3a9c49556470baf0f3e2e730"
 
   url "https://github.com/hewigovens/jayjay/releases/download/v#{version.csv.first}/JayJay-#{version.csv.first}.zip"
   name "JayJay"
