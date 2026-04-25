c1f694e1aaebe0634d0fa3ccfbb48feaa6ea62fd
hewigovens <360470+hewigovens@users.noreply.github.com>
2026-04-25 21:41:44 +0900

    Update ghosttile cask to 2.0.1,18

--- Casks/ghosttile.rb
+++ Casks/ghosttile.rb
@@ -1,6 +1,6 @@
 cask "ghosttile" do
-  version "2.0.0,17"
-  sha256 "6616673e2e80803f9433e311756b861711b95b6e2b64ca5ab255b6fe61275150"
+  version "2.0.1,18"
+  sha256 "a6338b7033dcd788f0a97eb4afc6055c5728e5ec803c78b582f725b51ee6d1e3"
 
   url "https://github.com/hewigovens/ghosttile-cli/releases/download/v#{version.csv.first}/GhostTile-#{version.csv.first}.zip"
   name "GhostTile"
