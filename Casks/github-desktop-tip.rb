16a69b0feba9d9d37c7972fee9925f605adfe3e6
hewigovens <360470+hewigovens@users.noreply.github.com>
2026-08-31 12:15:13 +0900

    Fix github-desktop-tip cask style

--- Casks/github-desktop-tip.rb
+++ Casks/github-desktop-tip.rb
@@ -4,14 +4,13 @@ cask "github-desktop-tip" do
 
   url "https://github.com/hewigovens/github-desktop/releases/download/tip-#{version}/GitHub.Desktop-arm64.zip"
   name "GitHub Desktop (Tip)"
-  desc "GitHub Desktop tip build from hewigovens/github-desktop"
+  desc "Tip build of GitHub Desktop from hewigovens/github-desktop"
   homepage "https://github.com/hewigovens/github-desktop"
 
+  conflicts_with cask: "github-desktop"
   depends_on macos: :monterey
 
   app "GitHub Desktop.app"
-
-  conflicts_with cask: "github-desktop"
 
   zap trash: [
     "~/Library/Application Support/GitHub Desktop",
